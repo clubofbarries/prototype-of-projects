e06f6fc8f2b4a39e8165ac3bede4b30d203ed71d
Syed Waseem <113202742+clubofbarries@users.noreply.github.com>
2023-07-24 12:18:21 +0530

    the Metro Fare system
    this is developed with n3 and n4

---
+++ Main.java
@@ -0,0 +1,25 @@
+import abs.MetroBill;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+     
+
+      MetroBill metroBill = new MetroBill();
+
+      System.out.println(metroBill.oneWay());
+
+      System.out.println(metroBill.roundTrip());
+
+      System.out.println(metroBill.womenPassenger());
+
+      System.out.println(metroBill.ChildPassenger());
+
+      }
+
+
+    }
+
+  
+    
